11f46c359ceb645cf7b764d5d1faf4903839c946
agent-bear <59876605+agent-bear@users.noreply.github.com>
2020-11-26 15:58:46 +0100

    .

--- test1.py
+++ test1.py
@@ -1,48 +1,79 @@
 from splinter import Browser
-from selenium.webdriver.common.keys import Keys
 import time
 import re
+import requests
+import json
+
 browser = Browser("chrome")
 
-targetUrl = "http://bedrijf-overzicht.startbewijs.nl/"
+linksToRequest = ["http://bedrijf-overzicht.startbewijs.nl/","http://bedrijf-overzicht.startbewijs.nl/"]
 email = "mathijs@marketingmadheads.com"
 title = "testpagina"
 url = "https://testpage.com/"
-desc = "lorem ipsum dolor sit amet"
+
+def requestLinks():
+    for targetUrl in linksToRequest:
+
+        #calls api request with current target url
+        getBackLinkURLs(targetUrl)
 
 
-domain = re.sub("http.*?://", "", targetUrl)
-subdomain = re.sub("\..*?/", "", domain)
-domain = re.sub("/", "", domain)
+        #modifies existing strings to fill form fields.
+        domain = re.sub("http.*?://", "", targetUrl)
+        subdomain = re.sub("\..*?/", "", domain)
+        domain = re.sub("/", "", domain)
+
+        #opens the link window and autofills everything
+        browser.windows.current = browser.windows[0]
+        browser.visit(targetUrl)
+        time.sleep(1)
+        submitButton = browser.find_by_text("Link aanmelden" or "Submit link")
+        submitButton.click()
+        time.sleep(1)
+        browser.find_by_name("email").fill(email)
+        time.sleep(0.1)
+        browser.find_by_name("pagename").fill(subdomain)
+        time.sleep(0.1)
+        browser.find_by_text(subdomain.capitalize()).click()
+        time.sleep(0.1)
+        browser.find_by_name("linktitel" or "linktitle").fill(title)
+        time.sleep(0.1)
+        browser.find_by_name("linkurl").fill(url)
 
 
+def getBackLinkURLs(target):
+    
+    # request for 10 backlinks from majestic OpenApp API
+    data = {
+    'accesstoken': "AGBKACBKAFBGH",
+    'privatekey': "",
+    'Timeout': 10,
+    'cmd': 'GetBackLinkData',
+    'item': target,
+    'datasource': 'fresh',
+    'Count': 10,
+    'Mode': 1,
+    };
+    
+    #Sends GET request to majestic API
+    r = requests.get('https://developer.majestic.com/api/json', data)
+    #r = requests.get('https://api.majestic.com/api/json', data)
 
-print(domain)
-print(subdomain)
+    #Filters response to get dictionary with just data from backlinks
+    response = json.loads(r.text)
+    response = response["DataTables"]
+    response = response["BackLinks"]
+    response = response["Data"]
 
+    #New empty array for Backlink URLs
+    backLinks = []
 
+    #goes through response dictionary and adds backlink urls to the new array
+    for i in range(len(response)):
+        link = response[i]
+        backLinks.append(link['SourceURL'])
 
-#opens the link window
-browser.windows.current = browser.windows[0]
-browser.visit(targetUrl)
-time.sleep(1)
-#navigates to the link submission page
-submitButton = browser.find_by_text("Link aanmelden" or "Submit link")
-submitButton.click()
-time.sleep(1)
+    print(backLinks)
 
-browser.find_by_name("email").fill(email)
-time.sleep(0.1)
-browser.find_by_name("pagename").fill(subdomain)
-time.sleep(0.1)
-browser.find_by_text(subdomain.capitalize()).click()
-time.sleep(0.1)
-browser.find_by_name("linktitel" or "linktitle").fill(title)
-time.sleep(0.1)
-browser.find_by_name("linkurl").fill(url)
-time.sleep(2)
-browser.find_by_name("description").click()
-browser.find_by_name("description").type(desc)
-
-
-
+requestLinks()
+        
