9bfe2cdb50ecdf0e5a6ff0e67637b23670e84ae4
agent-bear <59876605+agent-bear@users.noreply.github.com>
2020-12-03 10:06:07 +0100

    .

--- test1.py
+++ test1.py
@@ -1,22 +1,37 @@
+#from flask import Flask
 from splinter import Browser
 import time
 import re
 import requests
 import json
 
+#flask init
+#app = Flask(__name__)
+
+#@app.route('/')
+#def index():
+#    return 'Hello World!'
+
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0")
+
 browser = Browser("chrome")
 
-linksToRequest = ["http://bedrijf-overzicht.startbewijs.nl/","http://bedrijf-overzicht.startbewijs.nl/"]
+linksToRequest = ["https://piano.allepaginas.nl/", "https://bedrijf-overzicht.startbewijs.nl/", ]
 email = "mathijs@marketingmadheads.com"
-title = "testpagina"
-url = "https://testpage.com/"
+title = "Pianowebsite"
+url = "https://pianowebsite.nl/online-pianoles/"
+
+
+
 
 def requestLinks():
     for targetUrl in linksToRequest:
 
         #calls api request with current target url
-        getBackLinkURLs(targetUrl)
+        backLinks = getBackLinkURLs(targetUrl)
 
+        print(backLinks)
 
         #modifies existing strings to fill form fields.
         domain = re.sub("http.*?://", "", targetUrl)
@@ -27,8 +42,16 @@ def requestLinks():
         browser.windows.current = browser.windows[0]
         browser.visit(targetUrl)
         time.sleep(1)
-        submitButton = browser.find_by_text("Link aanmelden" or "Submit link")
-        submitButton.click()
+
+        #tries all possible texts and click it if it finds a hit.
+        buttonText = ["submit Link", "Link aanmelden"]
+        for text in buttonText:
+            button = browser.find_by_value(text)
+            if len(button) > 0:
+                button.click()
+                break
+
+
         time.sleep(1)
         browser.find_by_name("email").fill(email)
         time.sleep(0.1)
@@ -39,6 +62,28 @@ def requestLinks():
         browser.find_by_name("linktitel" or "linktitle").fill(title)
         time.sleep(0.1)
         browser.find_by_name("linkurl").fill(url)
+
+        for link in backLinks:
+            #fills every backlink until one matches
+            browser.find_by_name("backlink").fill(link)
+            time.sleep(0.1)
+            browser.check("agreement")
+            time.sleep(0.1)
+
+            #tries all variants of the button until it gets one that matches.
+            buttonText = ["Request Link", "Vraag link aan"]
+            for text in buttonText:
+                button = browser.find_by_value(text)
+                if len(button) > 0:
+                    button.click()
+                    break
+            
+            time.sleep(2)
+
+            #breaks the for loop if the link gets accepted.
+            if browser.is_text_present("succesvol") == True:
+                print("link successvol aangevraagd!")
+                break
 
 
 def getBackLinkURLs(target):
@@ -56,8 +101,8 @@ def getBackLinkURLs(target):
     };
     
     #Sends GET request to majestic API
-    r = requests.get('https://developer.majestic.com/api/json', data)
-    #r = requests.get('https://api.majestic.com/api/json', data)
+    #r = requests.get('https://developer.majestic.com/api/json', data)
+    r = requests.get('https://api.majestic.com/api/json', data)
 
     #Filters response to get dictionary with just data from backlinks
     response = json.loads(r.text)
@@ -73,7 +118,8 @@ def getBackLinkURLs(target):
         link = response[i]
         backLinks.append(link['SourceURL'])
 
-    print(backLinks)
+    return backLinks
 
 requestLinks()
+
         
